df61735f8b76fe8020f64a175824e01e37edd73f
K. C. Juntunen <kcjuntunen@frontier.com>
2016-02-05 13:41:50 -0500

    Unit testing

--- RedbrickTest/CutlistDataTest.cs
+++ RedbrickTest/CutlistDataTest.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 namespace RedbrickTest {
   [TestClass]
   public class CutlistDataTest {
+    CutlistData cd = new CutlistData();
     //[TestMethod]
     //public void GetECODataTest() {
     //  eco eco = cd.GetECOData("8042");
@@ -20,14 +21,12 @@ namespace RedbrickTest {
 
     [TestMethod]
     public void GetHashTest() {
-      CutlistData cd = new CutlistData();
       int i = cd.GetHash("ZASC1505-02-06");
       Debug.Assert(i == 246268142);
     }
 
     [TestMethod]
     public void GetLegacyECODataTest() {
-      CutlistData cd = new CutlistData();
       eco eco = cd.GetLegacyECOData("8807");
       Debug.Assert(eco.RequestedBy == "S.PALMER");
 
@@ -37,24 +36,22 @@ namespace RedbrickTest {
 
     [TestMethod]
     public void GetMaterialIDTest() {
-      CutlistData cd = new CutlistData();
       int res = cd.GetMaterialID("SHT SST 430 #4 POLISH 24GA");
-      Debug.Assert(res == 1311);
+      Debug.Assert(res == 1339);
 
       res = cd.GetMaterialID("TBD MATERIAL");
-      Debug.Assert(res == 3004);
+      Debug.Assert(res == 3030);
 
       res = cd.GetMaterialID("ksladhfgakldfhasd");
-      Debug.Assert(res == 3004);
+      Debug.Assert(res == 3030);
 
       res = cd.GetMaterialID(string.Empty);
-      Debug.Assert(res == 3004);
+      Debug.Assert(res == 3030);
     }
 
     [TestMethod]
     public void GetMaterialByIDTest() {
-      CutlistData cd = new CutlistData();
-      string res = cd.GetMaterialByID("1311");
+      string res = cd.GetMaterialByID("1339");
       Debug.Assert(res == "SHT SST 430 #4 POLISH 24GA");
 
       res = cd.GetMaterialByID("9000");
@@ -63,7 +60,6 @@ namespace RedbrickTest {
 
     [TestMethod]
     public void GetOpDataByNameTest() {
-      CutlistData cd = new CutlistData();
       System.Collections.Generic.List<string> l = new System.Collections.Generic.List<string>();
       l = cd.GetOpDataByName("NOT");
       Debug.Assert(l[0] == "20");
@@ -74,23 +70,9 @@ namespace RedbrickTest {
     }
 
     [TestMethod]
-    public void TestTypeEnum() {
-      Debug.Print(string.Format("{0}\n{1}\n{3}\n{4}n{5}",
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoDate,
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoDouble,
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoNumber,
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoText,
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoUnknown,
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoYesOrNo));
-      System.Console.Write(string.Format("{0}\n{1}\n{3}\n{4}n{5}",
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoDate,
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoDouble,
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoNumber,
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoText,
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoUnknown,
-        SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoYesOrNo));
-      Debug.Assert(SolidWorks.Interop.swconst.swCustomInfoType_e.swCustomInfoDate == 0);
-      System.Console.ReadKey();
+    public void TestGetLastLegacyECO() {
+      int x = cd.GetLastLegacyECR();
+      Debug.Assert(x >= 8845);
     }
   }
 }
--- RedbrickTest/UnitTest1.cs
+++ RedbrickTest/UnitTest1.cs
@@ -5,25 +5,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace RedbrickTest {
   [TestClass]
   public class UnitTest1 {
+    M2MData m2m = new M2MData();
     [TestMethod]
     public void M2MGetPartCount() {
-      M2MData m2m = new Redbrick_Addin.M2MData();
+      System.Diagnostics.Debug.Assert(m2m.GetPartCount("Z75794", "100") > 0);
+      System.Diagnostics.Debug.Assert(m2m.GetPartCount("083511", "100") > 0);
       System.Diagnostics.Debug.Assert(m2m.GetPartCount("TAFX1423-06", "100") == 1);
       System.Diagnostics.Debug.Assert(m2m.GetPartCount("BBBBB", "100") == 0);
     }
 
     [TestMethod]
     public void M2MGetPurchased() {
-      M2MData m2m = new Redbrick_Addin.M2MData();
-      System.Diagnostics.Debug.Assert(m2m.GetPurchased("083551", "100") == true);
+      System.Diagnostics.Debug.Assert(m2m.GetPurchased("083511", "100") == true);
       System.Diagnostics.Debug.Assert(m2m.GetPurchased("TAFX1423-06", "100") == false);
+      System.Diagnostics.Debug.Assert(m2m.GetPurchased("Z75794", "100") == false);
+      System.Diagnostics.Debug.Assert(m2m.GetPurchased("BBBBB", "100") == false);
+    }
+
+    [TestMethod]
+    public void M2MGetProductCl() {
+      System.Diagnostics.Debug.Assert(m2m.GetProductClass("Z75794", "100") == "02");
+      System.Diagnostics.Debug.Assert(m2m.GetProductClass("083511", "100") == "09");
+      System.Diagnostics.Debug.Assert(m2m.GetProductClass("TAFX1423-06", "100") == "09");
+      System.Diagnostics.Debug.Assert(m2m.GetProductClass("BBBBB", "100") == string.Empty);
     }
 
     [TestMethod]
     public void M2MGetPartType() {
-      M2MData m2m = new Redbrick_Addin.M2MData();
-      System.Diagnostics.Debug.Assert(m2m.GetPartType("083551", "100") == 2);
+      System.Diagnostics.Debug.Assert(m2m.GetPartType("Z75794", "100") == 3);
+      System.Diagnostics.Debug.Assert(m2m.GetPartType("083511", "100") == 2);
       System.Diagnostics.Debug.Assert(m2m.GetPartType("TAFX1423-06", "100") == 1);
+      System.Diagnostics.Debug.Assert(m2m.GetPartType("BBBBB", "100") == 7);
     }
   }
 }
