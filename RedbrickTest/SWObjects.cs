6df2c25073dd62896b3f44a0959d672948c18cff
K. C. Juntunen <kcjuntunen@frontier.com>
2016-04-22 07:55:17 -0400

    I'd like to do more complete testing,
    but I don't know how to unit test SolidWorks objects.

---
+++ RedbrickTest/SWObjects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolidWorks.Interop.swconst;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swpublished;
+using Redbrick_Addin;
+
+namespace RedbrickTest {
+  [TestClass]
+  public class SWObjects {
+    [TestMethod]
+    public void TestGetProps() {
+      //SwProperties s = new SwProperties(new SldWorks());
+      //s.Add(new SwProperty(new CustomPropertyManager(), "stuff", swCustomInfoType_e.swCustomInfoText, 
+      //  "other stuff", true));
+      //s.Add(new SwProperty(new CustomPropertyManager(), "thing", swCustomInfoType_e.swCustomInfoText, 
+      //  "other thing", false));
+
+      //Assert.AreEqual("other stuff", s.GetProperty("stuff").Value);
+      //Assert.AreEqual("other thing", s.GetProperty("thing").Value);
+      //Assert.IsTrue(s.Contains("stuff"));
+      //Assert.IsFalse(s.Contains("other"));
+    }
+  }
+}
---
+++ RedbrickTest/StaticFunctions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Redbrick_Addin;
+
+namespace RedbrickTest {
+  [TestClass]
+  public class StaticFunctions {
+    [TestMethod]
+    public void TestClip() {
+      Redbrick.Clip("hey");
+    }
+
+    [TestMethod]
+    public void TestGetHash() {
+      int x = Redbrick.GetHash(@"C:\");
+      Assert.IsTrue(-890689235 == x);
+    }
+
+    [TestMethod]
+    public void TestFilterString() {
+      string test = ";test%*" + '\u0022' + '\u0027';
+      string target1 = '\u037E' + "TEST" + '\u066A' + '\u2217' + '\u2033' + '\u2032';
+      string target2 = '\u037E' + "test" + '\u066A' + '\u2217' + '\u2033' + '\u2032';
+      string x = CutlistData.FilterString(test);
+
+      Assert.IsTrue(target1 == x);
+      x = CutlistData.FilterString(test, false);
+      Assert.IsTrue(target2 == x);
+      x = CutlistData.FilterString(test, true);
+      Assert.IsTrue(target1 == x);
+    }
+
+
+  }
+}
